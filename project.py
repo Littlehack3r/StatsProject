7d30f97c9b39adfb82438e47f0f3a61d1fd774a7
LittleHack3r <sxm6549@rit.edu>
2019-12-03 15:11:26 -0500

    init

--- project.py
+++ project.py
@@ -13,3 +13,24 @@ fnTrue = lambda x: 2+10*x-2*x**2
 
 def makeFakeData(fn):
        return fn(xvals)+np.random.normal(size=len(xvals))
+
+
+def outputData():
+       dat_out = []
+       for indx in np.arange(500):
+       dat_out.append(np.polyfit(xvals,makeFakeData(fn3),2))
+       dat_out = np.array(dat_out)
+       print dat_out
+       print np.mean(dat_out[:,0])
+       print np.mean(dat_out[:,1])
+       print np.std(dat_out[:,0])
+       print np.cov(dat_out[:,0],dat_out[:,1])
+
+plt.scatter(dat_out[:,0],dat_out[:,1])
+def variance():
+       print np.mean(dat_out[:,0])
+       print np.mean(dat_out[:,1])
+if __name__ == "__main__":
+    np.polyfit(xvals,makeFakeData(fn1),2)
+    outputData()
+    
